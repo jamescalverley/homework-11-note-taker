f56d68cab0a4445176fe6af2cc1fafced45f72f9
James Calverley <james.calverley3@gmail.com>
2020-03-21 19:08:09 -0400

    revises PORT

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
